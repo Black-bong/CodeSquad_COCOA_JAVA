52c700b1c830b09a1161916af635a49e126821a8
Black_Bong <shoy1415@naver.com>
2021-11-02 17:37:19 +0900

    while문 예제

--- week1/src/loop/WhileLoopEx.java
+++ week1/src/loop/WhileLoopEx.java
@@ -1,7 +1,33 @@
 package loop;
 
-public class WhileLoopEx {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class WhileLoopEx {
+    public static void main(String[] args) throws IOException {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+
+        int firstNumber = Integer.parseInt(buf.readLine());
+        System.out.println(cycle(firstNumber));
+    }
+
+    private static int cycle(int number) {
+        int cycleCount = 0;
+        int temp = number;
+
+        while (true) {
+            cycleCount++;
+            int value = separationOfNumbers(number);
+            if (temp == value) {
+                return cycleCount;
+            }
+            number = value;
+        }
+    }
+
+    private static int separationOfNumbers(int n) {
+        int value = (n / 10) + (n % 10);
+        return (n % 10 * 10) + (value % 10);
     }
 }
