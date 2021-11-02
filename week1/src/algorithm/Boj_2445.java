ca15a5e0c2f884c9fea3d4e3275e27b0190d1e74
Black_Bong <shoy1415@naver.com>
2021-11-02 10:31:39 +0900

    star 8

--- week1/src/algorithm/Boj_2445.java
+++ week1/src/algorithm/Boj_2445.java
@@ -1,7 +1,34 @@
 package algorithm;
 
+import java.util.Scanner;
+
 public class Boj_2445 {
     public static void main(String[] args) {
-        System.out.println("별찍기 8번");
+
+        Scanner scan = new Scanner(System.in);
+
+        int starCount = scan.nextInt();
+        int row = 2 * starCount -1;
+        int column = 2 * starCount;
+        int point = column / 2;
+        int start = 0;
+        int end = 0;
+
+        char[][] star = new char[row][column];
+
+        for (int i = 0; i < column; i++) {
+
+            if (i >= point) {
+                for (;start < row + end; start--) {
+                    star[start][i] = '*';
+                    end --;
+                }
+            } else {
+                for (;start < row - end; start++) {
+                    star[start][i] = '*';
+                    end ++;
+                }
+            }
+        }
     }
 }
