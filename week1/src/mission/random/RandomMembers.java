34d7800b5f32991da6add45e5e6751ce7a0aa911
Black_Bong <shoy1415@naver.com>
2021-11-03 13:25:15 +0900

    오타 수정

--- week1/src/mission/random/RandomMembers.java
+++ week1/src/mission/random/RandomMembers.java
@@ -6,28 +6,29 @@ import java.io.InputStreamReader;
 
 public class RandomMembers {
     public static void main(String[] args) throws IOException {
-
         RandomMembers randomMember = new RandomMembers();
 
         BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("몇 명인가요? ");
+
         int searchCount = Integer.parseInt(buf.readLine());
+
         buf.close();
 
         randomMember.outPutMembers(searchCount);
-
     }
 
     void outPutMembers(int count) {
         Member member = new Member();
-
         member.addMember();
 
         int length = member.getMembers().size();
 
         for (int i = 0; i < count; i++) {
+            int randomIndex = (int) (Math.random() * length);
+            String name = member.getMembers().get(randomIndex);
 
-            System.out.print(member.getMembers().get((int) (Math.random() * length)));
+            System.out.print(name);
             if (i == count - 1) {
                 break;
             }
