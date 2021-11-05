df65c4a55a790121af2ff491c96ed5c27920685c
Black_Bong <shoy1415@naver.com>
2021-11-05 16:20:11 +0900

    백준_1157_단어공부

---
+++ week1/src/algorithm/boj/Boj1157.java
@@ -0,0 +1,41 @@
+package algorithm.boj;
+import java.util.Scanner;
+
+public class Boj1157 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+        int[] alphabet = new int[26];
+        int index = 0;
+        int max = 0;
+        boolean flag = false;
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) < 91) {
+                alphabet[input.charAt(i) - 65]++;
+            }
+            if (input.charAt(i) > 96) {
+                alphabet[input.charAt(i) - 97]++;
+            }
+        }
+
+        for (int i = 0; i < alphabet.length; i++) {
+            if (alphabet[i] == max) {
+                flag = true;
+            }
+            if (alphabet[i] > max) {
+                max = alphabet[i];
+                index = i;
+                flag = false;
+            }
+        }
+
+        if (!flag) {
+            System.out.println((char) (65 + index));
+        }
+        if (flag) {
+            System.out.println("?");
+        }
+    }
+}
