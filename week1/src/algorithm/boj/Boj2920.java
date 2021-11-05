b2e9c579bec184e182c42468989f1ac6fd75cf32
Black_Bong <shoy1415@naver.com>
2021-11-05 14:40:58 +0900

    음계

---
+++ week1/src/algorithm/boj/Boj2920.java
@@ -0,0 +1,30 @@
+package algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Logger;
+
+public class Boj2920 {
+
+    private static final Logger LOG = Logger.getGlobal();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = buf.readLine().replace(" ", "");
+
+        LOG.info(input);
+
+        switch (input) {
+            case "12345678":
+                System.out.println("ascending");
+                break;
+            case "87654321":
+                System.out.println("descending");
+                break;
+            default:
+                System.out.println("mixed");
+        }
+    }
+}
