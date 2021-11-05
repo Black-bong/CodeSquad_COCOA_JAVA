ca514dea591f6e13a534a3f5718810a67fd47a06
Black_Bong <shoy1415@naver.com>
2021-11-05 14:09:53 +0900

    오전 알고리즘 풀이

---
+++ week1/src/algorithm/boj/Boj1546.java
@@ -0,0 +1,35 @@
+package algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
+
+public class Boj1546 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Double> scores = new ArrayList<>();
+
+        double avg = 0;
+        double temp = 0;
+        double sum = 0;
+        double max = 0;
+        int scoreCount = Integer.parseInt(buf.readLine());
+
+        StringTokenizer str = new StringTokenizer(buf.readLine(), " ");
+
+        for (int i = 0; i < scoreCount; i++) {
+            scores.add(Double.parseDouble(str.nextToken()));
+        }
+        max = scores.stream().mapToDouble(x -> x).max().orElseThrow(NoSuchElementException::new);
+        for (Double score : scores) {
+            temp = score / max * 100;
+            sum += temp;
+        }
+        avg = sum / scoreCount;
+        System.out.println(avg);
+    }
+}
