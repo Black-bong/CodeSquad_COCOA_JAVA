cce6dc187b4a5fdd7e0e2652e737b9d7e41befc1
Black_Bong <shoy1415@naver.com>
2021-11-08 22:17:16 +0900

    가계부_BufferedReader 싱글톤 구현

---
+++ week2/src/moneybook/BufferedInput.java
@@ -0,0 +1,16 @@
+package moneybook;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BufferedInput {
+
+    private static BufferedReader instance = new BufferedReader(new InputStreamReader(System.in));
+
+    public static BufferedReader getInstance() {
+        if (instance == null) {
+            instance = new BufferedReader(new InputStreamReader(System.in));
+        }
+        return instance;
+    }
+}
