99b655b43920f36d26f653b4fd53fb973f4f9771
Black_Bong <shoy1415@naver.com>
2021-11-08 14:45:38 +0900

    가계부_생성자 변경

--- week2/src/moneybook/MemberRepository.java
+++ week2/src/moneybook/MemberRepository.java
@@ -7,13 +7,24 @@ import java.util.Map;
 
 public class MemberRepository {
 
+    private static final MemberRepository instance = new MemberRepository();
     private static Map<Long , Member> members = new HashMap<>();
     private static long sequence = 0L;
+    private static boolean flag = false;
 
     public Member save(Member member) {
         member.setId(++sequence);
         members.put(sequence, member);
+        flag = true;
         return member;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public static MemberRepository getInstance() {
+        return instance;
     }
 
     public Member findByIdMember(Long id) {
