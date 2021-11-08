ddc5e970d542d9c3b3a4a75660e507b79fa08ae0
Black_Bong <shoy1415@naver.com>
2021-11-08 16:40:52 +0900

    가계부_오입력 예외처리 추가

--- week2/src/moneybook/Main.java
+++ week2/src/moneybook/Main.java
@@ -4,20 +4,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
+import java.util.logging.Logger;
 
 public class Main {
 
     DataRepository dataRepository = DataRepository.getInstance();
     MemberRepository memberRepository = MemberRepository.getInstance();
+    private static final Logger LOG = Logger.getGlobal();
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
         System.out.println("========가계부========");
-        main.selectMenu();
+        main.mainMenu();
     }
 
-    private void selectMenu() throws IOException {
+    private void mainMenu() throws IOException {
         System.out.println("========Menu========");
         System.out.println("1. 사용자 등록");
         System.out.println("2. 로그인");
@@ -33,13 +36,14 @@ public class Main {
                 dataMenu();
                 break;
             case 3:
+                return;
         }
     }
 
     private void dataMenu() throws IOException {
         if (!memberRepository.isFlag()) {
             System.out.println("사용자를 등록해주세요.");
-            selectMenu();
+            memberData();
         }
         System.out.println("========Menu========");
         System.out.println("1. 데이터 등록");
@@ -61,29 +65,39 @@ public class Main {
                 updateData();
                 break;
             case 4:
+                dataDelete();
                 break;
             case 5:
-                dataDelete();
+                mainMenu();
                 break;
         }
     }
 
     private void createData() throws IOException {
 
-        System.out.println("날짜, 적요, 수입, 지출을 띄어쓰기로 구분하여 입력하시오.");
+        System.out.println("날짜, 적요, 수입, 지출을 띄어쓰기로 구분하여 입력하시오.(종료:exit)");
+        String date;
+
         System.out.print(">> ");
         BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer str = new StringTokenizer(buf.readLine(), " ");
 
-        String date = str.nextToken();
-        String briefs = str.nextToken();
-        double income = Double.parseDouble(str.nextToken());
-        double expenses = Double.parseDouble(str.nextToken());
-
-        Data data = new Data(date, briefs, income, expenses);
-
-        dataRepository.dataSave(data);
-        dataMenu();
+        try {
+            date = str.nextToken();
+            if (date.equals("exit")) {
+                dataMenu();
+            }
+            String briefs = str.nextToken();
+            double income = Double.parseDouble(str.nextToken());
+            double expenses = Double.parseDouble(str.nextToken());
+            Data data = new Data(date, briefs, income, expenses);
+            dataRepository.dataSave(data);
+            createData();
+        } catch (NoSuchElementException e) {
+            System.out.println("입력값 오류 다시 입력해주세요.");
+            LOG.info(e.toString());
+            createData();
+        }
     }
 
     private void readData() throws IOException {
@@ -94,7 +108,7 @@ public class Main {
         List<Data> data = dataRepository.findAllData();
 
         for (Data d : data) {
-            if (d.getDate().substring(0, 2).equals(date)) {
+            if (d.getDate().substring(0, 3).equals(date)) {
                 System.out.println(d.toString());
             }
         }
@@ -102,12 +116,20 @@ public class Main {
     }
 
     private void updateData() throws IOException {
-        System.out.println("수정할 데이터의 번호를 입력하시오.");
+        System.out.println("수정할 데이터의 번호를 입력하시오.(종료:exit)");
         System.out.print(">> ");
 
         BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
         Long id = Long.parseLong(buf.readLine());
-        dataRepository.findByIdData(id);
+        System.out.println("날짜, 적요, 수입, 지출을 띄어쓰기로 구분하여 입력하시오.");
+        System.out.print(">> ");
+        StringTokenizer str = new StringTokenizer(buf.readLine(), " ");
+        Data data = dataRepository.findByIdData(id);
+        data.setDate(str.nextToken());
+        data.setBriefs(str.nextToken());
+        data.setIncome(Double.parseDouble(str.nextToken()));
+        data.setExpenses(Double.parseDouble(str.nextToken()));
+
         dataMenu();
     }
 
@@ -122,7 +144,7 @@ public class Main {
     }
 
     private void memberData() throws IOException {
-        System.out.println("사용자 이름과 비밀번호를 입력하세요.");
+        System.out.println("사용자 이름과 비밀번호를 띄어쓰기로 구분하여 입력하세요.");
         System.out.print(">> ");
         BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer str = new StringTokenizer(buf.readLine(), " ");
@@ -135,6 +157,6 @@ public class Main {
     private void createMember(String u, String p) throws IOException {
         Member member = new Member(u, p);
         memberRepository.save(member);
-        selectMenu();
+        mainMenu();
     }
 }
