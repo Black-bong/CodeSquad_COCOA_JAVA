60c12e2465ebd5945b34ecbef742f9ed00dac7fc
Black_Bong <shoy1415@naver.com>
2021-11-08 14:45:04 +0900

    가계부_잔액계산 테스트 추가

--- week2/src/moneybook/DataRepositoryTest.java
+++ week2/src/moneybook/DataRepositoryTest.java
@@ -18,12 +18,27 @@ class DataRepositoryTest {
     @Test
     @DisplayName("데이터 저장 확인")
     void saveData() {
-        Data data = new Data(LocalDateTime.now(),"핸드폰요금",10000.0,0.0);
+        Data data = new Data("11월02일","핸드폰요금",10000.0,0.0);
         DataRepository dataRepository = new DataRepository();
         dataRepository.dataSave(data);
 
         System.out.println(data.getDate());
 
         Assertions.assertEquals(data.getBriefs(),"핸드폰요금");
+    }
+
+    @Test
+    @DisplayName("잔액 호출")
+    void callCash() {
+        DataRepository dataRepository = new DataRepository();
+        Data data1 = new Data("11월01일", "월급", 1000000.0, 0.0);
+        dataRepository.dataSave(data1);
+        Data data2 = new Data("11월02일", "핸드폰요금", 0.0, 100000.0);
+        dataRepository.dataSave(data2);
+        Data data3 = new Data("11월03일", "보너스", 120000.0, 0.0);
+        dataRepository.dataSave(data3);
+
+        Assertions.assertEquals(data3.getCash(), 1020000.0);
+        dataRepository.findAllData();
     }
 }
