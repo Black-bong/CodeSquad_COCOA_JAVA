b3e4ad89374d0d70d38a2235c0d83cbb63855a15
Black_Bong <shoy1415@naver.com>
2021-11-09 23:35:24 +0900

    가계부_컨트롤러 생성

---
+++ week2/src/moneybook/controller/MenuController.java
@@ -0,0 +1,51 @@
+package moneybook.controller;
+
+import moneybook.BufferedReaderSingleton;
+import moneybook.model.MainMenu;
+import moneybook.model.SubMenu;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class MenuController {
+
+    private int menuNumber;
+    BufferedReader bufferedReader = BufferedReaderSingleton.getInstance();
+
+    public void selectMainMenus() throws IOException {
+        MainMenu menu = new MainMenu();
+        this.menuNumber = Integer.parseInt(bufferedReader.readLine());
+        switch (menuNumber) {
+            case 1:
+                menu.createAccount();
+                break;
+            case 2:
+                menu.login();
+                break;
+            case 3:
+                menu.exit();
+        }
+    }
+
+    public void selectSubMenus() throws IOException {
+        SubMenu subMenu = new SubMenu();
+        this.menuNumber = Integer.parseInt(bufferedReader.readLine());
+        switch (menuNumber) {
+            case 1:
+                subMenu.createData();
+                break;
+            case 2:
+                subMenu.readData();
+                break;
+            case 3:
+                subMenu.updateData();
+                break;
+            case 4:
+                subMenu.dataDelete();
+                break;
+            case 5:
+                subMenu.goToMainMenu();
+                break;
+        }
+    }
+}
