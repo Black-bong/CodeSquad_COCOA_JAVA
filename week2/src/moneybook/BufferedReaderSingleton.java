5f614c6aa2ac01fef92c34346096b8110bb3e892
Black_Bong <shoy1415@naver.com>
2021-11-09 23:31:01 +0900

    가계부_클래스명 변경

--- week2/src/moneybook/BufferedReaderSingleton.java
+++ week2/src/moneybook/BufferedReaderSingleton.java
@@ -3,14 +3,11 @@ package moneybook;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class BufferedInput {
+public class BufferedReaderSingleton {
 
     private static final BufferedReader instance = new BufferedReader(new InputStreamReader(System.in));
 
     public static BufferedReader getInstance() {
-//        if (instance == null) {
-//            instance = new BufferedReader(new InputStreamReader(System.in));
-//        }
         return instance;
     }
 }
