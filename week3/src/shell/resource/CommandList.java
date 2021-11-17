aeee6398e88631df90ee6ea601e3680723449050
Black_Bong <shoy1415@naver.com>
2021-11-18 00:08:47 +0900

    셸만들기_종료 명령어 추가

--- week3/src/shell/resource/CommandList.java
+++ week3/src/shell/resource/CommandList.java
@@ -7,7 +7,8 @@ public enum CommandList {
     C3("rm", 2),
     C4("cd", 3),
     C5("ls", 4),
-    C6("clock", 5);
+    C6("clock", 5),
+    C7("exit", 6);
 
     private final String command;
     private final int commandID;
--- week3/src/shell/view/ShellMain.java
+++ week3/src/shell/view/ShellMain.java
@@ -10,7 +10,7 @@ public class ShellMain {
     public void shellMainScreen() throws IOException {
         Input input = new Input();
         CommandController commandController = new CommandController();
-        System.out.print("B_Bong Java Shell >");
+        System.out.print("B_Bong Java Shell> ");
         String inputString = input.string();
 
         try {
