af492370d11d84ff2979623d1bab8696c0c6f6c0
Black_Bong <shoy1415@naver.com>
2021-11-18 00:10:03 +0900

    셸만들기_명령어 로그 출력

--- week3/src/shell/model/CommandProgress.java
+++ week3/src/shell/model/CommandProgress.java
@@ -1,24 +1,36 @@
 package shell.model;
 
+import shell.view.ShellMain;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
 public class CommandProgress {
+    private static final Logger LOG = Logger.getGlobal();
+    ShellMain shellMain = new ShellMain();
 
-    public void mkdir() {
-
+    public void mkdir() throws IOException {
+        LOG.info("mkdir 명령어");
+        shellMain.shellMainScreen();
     }
 
-    public void ls() {
-
+    public void ls() throws IOException {
+        LOG.info("ls 명령어");
+        shellMain.shellMainScreen();
     }
 
-    public void cd() {
-
+    public void cd() throws IOException {
+        LOG.info("cd 명령어");
+        shellMain.shellMainScreen();
     }
 
-    public void rm() {
-
+    public void rm() throws IOException {
+        LOG.info("rm 명령어");
+        shellMain.shellMainScreen();
     }
 
-    public void pwd() {
-
+    public void pwd() throws IOException {
+        LOG.info("pwd 명령어");
+        shellMain.shellMainScreen();
     }
 }
