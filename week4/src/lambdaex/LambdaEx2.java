088a0d04db61df4cb5e76a9e0f3c4d2b9f5eed7e
Black_Bong <shoy1415@naver.com>
2021-11-23 22:12:44 +0900

    람다_람다 예제

---
+++ week4/src/lambdaex/LambdaEx2.java
@@ -0,0 +1,48 @@
+package lambdaex;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.*;
+
+public class LambdaEx2 {
+
+    public static void main(String[] args) {
+        Supplier<Integer> s = () -> (int)(Math.random() * 100) + 1;
+        Consumer<Integer> c = i -> System.out.print(i + ", ");
+        Predicate<Integer> p = i -> i % 2 == 0;
+        UnaryOperator<Integer> f = i -> i / 10 * 10;
+
+        List<Integer> list = new ArrayList<>();
+        makeRandomList(s, list);
+        System.out.println(list);
+        printEvenNum(p, c, list);
+        List<Integer> newList = doSomething(f, list);
+        System.out.println(newList);
+    }
+
+    static <T> List<T> doSomething(UnaryOperator<T> f, List<T> list) {
+        List<T> newList = new ArrayList<>(list.size());
+
+        for (var i : list) {
+            newList.add(f.apply(i));
+        }
+        return newList;
+    }
+
+    static <T> void printEvenNum(Predicate<T> p, Consumer<T> c, List<T> list) {
+        System.out.print("[");
+        for (var i : list) {
+            if(p.test(i))
+                c.accept(i);
+        }
+        System.out.println("]");
+    }
+
+    static <T> void makeRandomList(Supplier<T> s, List<T> list) {
+        for (int i = 0; i < 10; i ++) {
+            list.add(s.get());
+        }
+    }
+
+}
