5dcf6e51566693c33bbcf6415261790bff140039
Black_Bong <shoy1415@naver.com>
2021-12-01 10:10:12 +0900

    Rubik's큐브_명령어 입력창 출력 메소드 생성, 메인 메소드 수정

--- week5/src/testpreparation/step3/RubiksCubeMain.java
+++ week5/src/testpreparation/step3/RubiksCubeMain.java
@@ -1,12 +1,19 @@
 package testpreparation.step3;
 
+import testpreparation.step3.controller.Commend;
+import testpreparation.step3.model.RubiksCube;
 import testpreparation.step3.view.PrintScreen;
 
 import java.io.IOException;
 
+
 public class RubiksCubeMain {
     public static void main(String[] args) throws IOException {
+        RubiksCube rubiksCube = new RubiksCube();
         PrintScreen printScreen = new PrintScreen();
-        printScreen.inputScreen();
+        Commend commend = new Commend();
+        rubiksCube.start();
+        printScreen.startScreen(rubiksCube);
+        commend.inputCommend(printScreen);
     }
 }
--- week5/src/testpreparation/step3/view/PrintScreen.java
+++ week5/src/testpreparation/step3/view/PrintScreen.java
@@ -6,15 +6,16 @@ import testpreparation.step3.model.RubiksCube;
 
 public class PrintScreen {
 
-    public void inputScreen() {
-        RubiksCube rubiksCube = new RubiksCube();
-        rubiksCube.start();
+    public void startScreen(RubiksCube rubiksCube) {
         rubiksCube.printCube();
-        System.out.print("CUBE> ");
     }
 
     public void endScreen() {
         System.out.println("이용해주셔서 감사합니다. 뚜뚜뚜.");
         System.exit(0);
+    }
+
+    public void inputBar() {
+        System.out.print("CUBE> ");
     }
 }
