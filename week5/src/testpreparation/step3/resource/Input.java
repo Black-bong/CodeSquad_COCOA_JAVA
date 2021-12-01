7366a1e082d37df3c23131a215647ca675661935
Black_Bong <shoy1415@naver.com>
2021-12-01 14:04:14 +0900

    Rubik's큐브_사용자 명령어 입력 시 숫자, 특수문자 구분 기능 추가

--- week5/src/testpreparation/step3/resource/Input.java
+++ week5/src/testpreparation/step3/resource/Input.java
@@ -3,10 +3,35 @@ package testpreparation.step3.resource;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 public class Input {
-    public String inputString() throws IOException {
+    public void inputString(List<String> commandList) throws IOException {
         BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
-        return buf.readLine().toUpperCase();
+        String commands = buf.readLine().toUpperCase();
+        splitString(commands, commandList);
+    }
+
+    private void splitString(String commands, List<String> commandList) {
+        for (int i = 0; i < commands.length(); i++) {
+            int lastIndex = commandList.size() - 1;
+            String command = Character.toString(commands.charAt(i));
+            if (command.equals("'")) {
+                commandList.set(lastIndex, commandList.get(lastIndex) + "'");
+                continue;
+            }
+            isInteger(command, commandList);
+        }
+    }
+
+    private void isInteger(String command, List<String> commandList) {
+        int lastIndex = commandList.size() - 1;
+        try {
+            for (int j = 0; j < Integer.parseInt(command) - 1; j++) {
+                commandList.add(commandList.get(lastIndex));
+            }
+        } catch (NumberFormatException e) {
+            commandList.add(command);
+        }
     }
 }
