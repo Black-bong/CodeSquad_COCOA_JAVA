a2d0203ebf93e8bf9bf758a4814a061a555ab2f2
Black_Bong <shoy1415@naver.com>
2021-12-04 02:51:28 +0900

    숫자야구_Number클래스 전역변수 제거

--- week5/src/testpreparation/step4/Number.java
+++ week5/src/testpreparation/step4/Number.java
@@ -5,29 +5,21 @@ import java.util.List;
 
 public class Number {
 
-    private static final List<Integer> numberList = new ArrayList<>();
-
-    public Number(int startNum, int endNum) {
-        for (int i = startNum; i < endNum + 1; i++) {
-            numberList.add(i);
-        }
-    }
-
-    public List<Integer> getNumber(int numberCount) {
+    public List<Integer> getNumber(List<Integer> numberList, int numberCount) {
         List<Integer> numbers = new ArrayList<>();
-        shuffleNumberList();
         for (int i = 0; i < numberCount; i++) {
-            numbers.add(numberList.get(i));
+            numbers.add(shuffleNumberList(numberList).get(i));
         }
         return numbers;
     }
 
-    private void shuffleNumberList() {
+    private List<Integer> shuffleNumberList(List<Integer> numberList) {
         for (int i = 0; i < numberList.size(); i++) {
             int randomIndex = (int) (Math.random() * numberList.size());
             int temp = numberList.get(randomIndex);
             numberList.set(randomIndex, numberList.get(i));
             numberList.set(i, temp);
         }
+        return numberList;
     }
 }
