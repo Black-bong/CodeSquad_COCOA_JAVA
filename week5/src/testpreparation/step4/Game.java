44f13e0b3cb2b61930297d9713e737b5745a849d
Black_Bong <shoy1415@naver.com>
2021-12-04 04:15:01 +0900

    숫자야구_Game클래스 전역변수 제거를 위한 리펙토링

--- week5/src/testpreparation/step4/Game.java
+++ week5/src/testpreparation/step4/Game.java
@@ -7,34 +7,41 @@ import java.util.List;
 public class Game {
 
     public void start() throws IOException {
-        BaseBall baseBall = new BaseBall();
-        randomNumber(baseBall);
-        inputNumber(baseBall);
-    }
-
-    private void inputNumber(BaseBall baseBall) throws IOException {
         PrintScreen printScreen = new PrintScreen();
-        Input input = new Input();
+        int count = 0;
+        int maxCount = 9;
         List<Integer> inputNumbers;
-        int inputCount = 0;
-        while (inputCount < 9) {
-            inputNumbers = (input.inputInteger(printScreen));
-            baseBall.saveInputNumbers(inputNumbers);
-            inputCount++;
+        List<Integer> randomNumbers = randomNumber();
+        while (count < maxCount) {
+            inputNumbers = inputNumber(printScreen);
+            saveNumber(inputNumbers, randomNumbers);
+            count++;
         }
         printScreen.gameOverScreen();
     }
 
-    private void randomNumber(BaseBall baseBall) {
+    private List<Integer> inputNumber(PrintScreen printScreen) throws IOException {
+        Input input = new Input();
+        List<Integer> inputNumbers;
+        inputNumbers = (input.inputInteger(printScreen));
+        return inputNumbers;
+    }
+
+    private List<Integer> randomNumber() {
         Number number = new Number();
         List<Integer> numberList = new ArrayList<>();
         int startNum = 1;
-        int endNum = 9;
+        int endNum = 10;
         int numberCount = 3;
-        for (int i = startNum; i < endNum + 1; i++) {
+        for (int i = startNum; i < endNum; i++) {
             numberList.add(i);
         }
         numberList = number.getNumber(numberList, numberCount);
-        baseBall.saveRandomNumbers(numberList);
+        return numberList;
+    }
+
+    private void saveNumber(List<Integer> inputNumbers, List<Integer> randomNumbers) {
+        BaseBall baseBall = new BaseBall();
+        baseBall.saveNumbers(inputNumbers, randomNumbers);
     }
 }
