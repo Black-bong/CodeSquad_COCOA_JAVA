77742c90159899ec1eed275d522a9480a5458051
Black_Bong <shoy1415@naver.com>
2021-12-03 12:44:42 +0900

    숫자야구_셔플, getNumber 구현

---
+++ week5/src/testpreparation/step4/Number.java
@@ -0,0 +1,35 @@
+package testpreparation.step4;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Number {
+
+    public static final List<Integer> numberList = new ArrayList<>();
+
+    public Number(int startNum, int endNum) {
+        for (int i = startNum; i < endNum + 1; i++) {
+            numberList.add(i);
+        }
+    }
+
+    public Set<Integer> getNumber(int numberCount) {
+        Set<Integer> numbers = new HashSet<>();
+        shuffleNumberList();
+        for (int i = 0; i < numberCount; i++) {
+            numbers.add(numberList.get(i));
+        }
+        return numbers;
+    }
+
+    private void shuffleNumberList() {
+        for (int i = 0; i < numberList.size(); i++) {
+            int randomIndex = (int) (Math.random() * numberList.size());
+            int temp = numberList.get(randomIndex);
+            numberList.set(randomIndex, numberList.get(i));
+            numberList.set(i, temp);
+        }
+    }
+}
