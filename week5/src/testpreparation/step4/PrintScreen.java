cdda47eb6139c1267617cf458647aff5b0e7f557
Black_Bong <shoy1415@naver.com>
2021-12-04 04:15:40 +0900

    숫자야구_화면 출력 메소드 추가

---
+++ week5/src/testpreparation/step4/PrintScreen.java
@@ -0,0 +1,30 @@
+package testpreparation.step4;
+
+public class PrintScreen {
+    public void inputBar() {
+        System.out.print("숫자를 입력해주세요 ex)123 :");
+    }
+
+    public void reInputScreen() {
+        System.out.println("잘못된 값입니다.");
+    }
+
+    public void gameOverScreen() {
+        System.out.println("기회를 모두 사용했습니다! 실패");
+    }
+
+    public void strikeAndBallScreen(int strike, int ball) {
+        if (strike != 0 || ball != 0) {
+            System.out.println(strike + "스트라이크! " + ball + "볼!");
+        }
+    }
+
+    public void notingScreen() {
+        System.out.println("낫싱!");
+    }
+
+    public void winGameScreen(int numberCount) {
+        System.out.println(numberCount + "개의 숫자를 모두 맞히셨습니다! 게임종료");
+        System.exit(0);
+    }
+}
